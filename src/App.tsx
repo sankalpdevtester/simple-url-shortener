19dc844cf50816bb6f3d97568cbe204fdba34188
sankalpdevtester <ohofufic0@gmail.com>
2026-06-21 10:32:00 +0530

    feat: add app component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShortenUrl from './ShortenUrl';
+
+const App = () => {
+  return (
+    <div>
+      <h1>URL Shortener</h1>
+      <ShortenUrl />
+    </div>
+  );
+};
+
+export default App;
